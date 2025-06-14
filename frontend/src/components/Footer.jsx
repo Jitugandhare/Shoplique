2f75bc7e122fefbaa044029a2cfff5e0b4af23b9
Jitu gandhare <jitugandhare@gmail.com>
2025-06-14 18:39:57 +0530

    frontend setup and footer section is completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Phone, Mail, GitHub, LinkedIn, YouTube, Instagram, Facebook } from '@mui/icons-material';
+
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className="footer-container">
+
+        <div className="footer-section contact">
+          <h3>Contact Us</h3>
+          <p><Phone fontSize="small" />Phone:+911234567890</p>
+          <p><Mail fontSize="small" />Email: 27jitug@gmail.com</p>
+        </div>
+
+        <div className="footer-section social">
+          <h3>Follow Us</h3>
+          <div className="social-links">
+            <a href="" target='_blank'>
+              <GitHub className='social-icons' />
+            </a>
+            <a href="" target='_blank'>
+              <LinkedIn className='social-icons' />
+            </a>
+            <a href="" target='_blank'>
+              <YouTube className='social-icons' />
+            </a>
+            <a href="" target='_blank'>
+              <Instagram className='social-icons' />
+            </a>
+            <a href="" target='_blank'>
+              <Facebook className='social-icons' />
+            </a>
+          </div>
+        </div>
+
+        <div className="footer-section about">
+          <h3>About Us</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique rem nobis iusto temporibus eius modi officia illo odio! No.</p>
+        </div>
+
+        <div className="footer-section bottom">
+          <p>&copy: Jitu Gandhare- All rights reserved
+
+          </p>
+        </div>
+
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+
+    <>
+      <div className="home-container">
+        <h2 className="home-heading">Trending Now</h2>
+      </div>
+      <Footer />
+    </>
+
+  )
+}
+
+export default Home
