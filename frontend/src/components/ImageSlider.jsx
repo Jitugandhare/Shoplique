4d6de07904ebd185b2e129564aabd69f64487de3
Jitu gandhare <jitugandhare@gmail.com>
2025-06-20 11:13:55 +0530

    Image slider is done

---
+++ frontend/src/components/ImageSlider.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import "../componentsStyles/ImageSlider.css"
+
+const images = [
+    './Images/Image-1.avif',
+    './Images/Image-2.avif',
+    './Images/Image-3.avif',
+    './Images/Image-4.avif',
+    './Images/Image-5.avif',
+
+
+];
+
+const ImageSlider = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length)
+        }, 5000)
+
+
+        return () => clearInterval(interval)
+
+    }, [])
+
+
+    return (
+        <div className="image-slider-container">
+            <div className="slider-images" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                {
+                    images.map((image, index) => (
+                        <div className="slider-item" key={index} >
+                            <img src={image} alt={`Slide ${index + 1}`} />
+                        </div>
+                    ))
+                }
+
+            </div>
+            <div className="slider-dots">
+                {
+                    images.map((__dirname, index) => (
+                        <span className={`dot ${index === currentIndex}? "active":""`} key={index}
+
+                            onClick={() => setCurrentIndex(index)}
+
+                        />
+
+                    ))
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default ImageSlider
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+
+
 import Footer from '../components/Footer'
+import ImageSlider from '../components/ImageSlider'
 
 const Home = () => {
   return (
 
     <>
       <NavBar />
+      <ImageSlider />
       <div className="home-container">
         <h2 className="home-heading">Trending Now</h2>
       </div>
