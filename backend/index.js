f2d9d90cb3fd3b9e54aa601677076f681016365f
Jitu gandhare <jitugandhare@gmail.com>
2025-06-21 17:38:03 +0530

    Diasplay api data from fetching server and connect backend with frontend

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ const connection = require('./config/db.js');
 const productRoute = require('./routes/productRoute.js')
 const userRoute = require('./routes/userRoute.js');
 const orderRoute = require('./routes/orderRoute.js');
+const cors=require("cors")
 
 process.on('uncaughtException', (err) => {
     console.log(`Error : ${err.message}`)
@@ -15,14 +16,14 @@ process.on('uncaughtException', (err) => {
     process.exit(1);
 })
 
-
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/user', userRoute)
-app.use('/products', productRoute)
-app.use('/order',orderRoute)
+app.use('/api/v1/user', userRoute)
+app.use('/api/v1/products', productRoute)
+app.use('/api/v1/order',orderRoute)
 
 
 app.listen(port, async () => {
--- frontend/src/features/product/productSlice.js
+++ frontend/src/features/product/productSlice.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 
 export const getProduct = createAsyncThunk('product/getProduct', async (_, { rejectWithValue }) => {
     try {
-        const link = '/products/';
+        const link = '/api/v1/products';
         const { data } = await axios.get(link)
-        console.log("Response", data);
+        console.log("Response data", data);
         return data;
     } catch (error) {
         return rejectWithValue(error.response?.data || 'An error occur')
