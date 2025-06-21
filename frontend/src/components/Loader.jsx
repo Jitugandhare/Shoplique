e4404bf14b0b7e97cea6147c4dbedaeee2f3e734
Jitu gandhare <jitugandhare@gmail.com>
2025-06-21 18:08:17 +0530

    adding loader and error handling

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../componentsStyles/Loader.css'
+
+
+const Loader = () => {
+  return (
+    <div className="loader-container">
+        <div className="loader">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Loader
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,44 +1,53 @@
 import React, { useEffect } from 'react'
 import NavBar from '../components/NavBar'
 import "../pageStyles/Home.css"
-
 import Footer from '../components/Footer'
 import ImageSlider from '../components/ImageSlider'
 import Product from '../components/Product'
 import PageTitle from '../components/PageTitle'
 import { useDispatch, useSelector } from 'react-redux'
-import { getProduct } from '../features/product/productSlice'
-
+import { getProduct, removeError } from '../features/product/productSlice'
+import Loader from '../components/Loader'
+import { toast } from "react-toastify"
 
 const Home = () => {
-    const { products, loading, error, productCount } = useSelector((state) => state.product) || [];
+    const { products, loading, error, productCount } = useSelector((state) => state.product) || {};
     const dispatch = useDispatch();
 
     useEffect(() => {
         dispatch(getProduct())
-    }, [dispatch])
+    }, [dispatch]);
+
+
+    useEffect(() => {
+        if (error) {
+            toast.error(error.message, { position: "top-center", autoClose: 3000 })
+        }
+        dispatch(removeError())
+    }, [dispatch, error])
 
     return (
-
         <>
-            <NavBar />
-            <PageTitle title="my-website" />
-            <ImageSlider />
-            <div className="home-container">
-                <h2 className="home-heading">Trending Now</h2>
-                <div className="home-product-container">
-                    {
-                        products?.map((product) => (
-                            <Product key={product._id}
-                                product={product}
-                            />
-                        ))
-                    }
-                </div>
-            </div>
-            {/* <Footer /> */}
+            {loading ? (
+                <Loader />
+            ) : (
+                <>
+                    <NavBar />
+                    <PageTitle title="my-website" />
+                    <ImageSlider />
+                    <div className="home-container">
+                        <h2 className="home-heading">Trending Now</h2>
+                        {error && <div className="error-message">{error}</div>}
+                        <div className="home-product-container">
+                            {products?.map((product) => (
+                                <Product key={product._id} product={product} />
+                            ))}
+                        </div>
+                    </div>
+                    {/* <Footer /> */}
+                </>
+            )}
         </>
-
     )
 }
 
