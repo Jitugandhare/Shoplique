3566b5e91891a4693b8e36402ca9410f14432268
Jitu gandhare <jitugandhare@gmail.com>
2025-07-05 09:53:46 +0530

    code refactor and added All routes seperately

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import ProductDetails from './pages/ProductDetails'
-import Products from './pages/Products'
-import Register from './User/Register'
+import AllRoutes from './Routes/AllRoutes'
+
 
 
 const App = () => {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/product-details/:id' element={<ProductDetails />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='/products/:keyword' element={<Products />} />
-          <Route path='/register' element={<Register />} />
-
-        </Routes>
-      </Router>
+      <AllRoutes />
     </>
   )
 }
---
+++ frontend/src/Routes/AllRoutes.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home'
+import ProductDetails from '../pages/ProductDetails'
+import Products from '../pages/Products'
+import Register from '../User/Register'
+
+
+const AllRoutes = () => {
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/product-details/:id' element={<ProductDetails />} />
+                    <Route path='/products' element={<Products />} />
+                    <Route path='/products/:keyword' element={<Products />} />
+                    <Route path='/register' element={<Register />} />
+
+                </Routes>
+            </Router>
+        </div>
+    )
+}
+
+export default AllRoutes
