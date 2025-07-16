aafdccb0c69f83e73781d623da9ab7543afe753a
Jitu gandhare <jitugandhare@gmail.com>
2025-07-16 10:29:54 +0530

    Render Cart Items & Update Quantity

--- frontend/src/cart/CartItem.jsx
+++ frontend/src/cart/CartItem.jsx
@@ -1,17 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../cartStyles/Cart.css';
+import { toast } from "react-toastify"
+import { addItemsToCart, removeError, removeSuccess } from "../features/cart/cartSlice"
+import { useDispatch, useSelector } from 'react-redux';
 
 const CartItem = ({ item }) => {
 
   console.log(item)
-  const [quantity, setQuantity] = useState(1)
+  const [quantity, setQuantity] = useState(item.quantity)
+  const { loading, error, success, message, cartItems } = useSelector((state) => state.cart)
+  const dispatch = useDispatch();
 
   const increaseQuantity = () => {
+    if (item.stock <= quantity) {
+      toast.error("Cannot exceeds available stocks", { position: "top-center", autoClose: 3000 });
+      dispatch(removeError());
+      return;
+    }
     setQuantity(qty => qty + 1)
   }
   const decreaseQuantity = () => {
-    setQuantity(qty => (qty > 1 ? qty - 1 : 1))
+    if (quantity <= 1) {
+      toast.error("Quantity cannot be less than 1", { position: "top-center", autoClose: 3000 });
+      dispatch(removeError());
+      return;
+    }
+    setQuantity(qty => qty - 1)
   }
+  // quantity update
+  const handleUpdateQuantity = () => {
+    if (loading) {
+      return
+    }
+
+    if (quantity !== item.quantity) {
+      dispatch(addItemsToCart({ id: item.product, quantity }))
+    }
+  }
+
+
+  useEffect(() => {
+    if (success) {
+      toast.success("Quantity updated", { position: "top-center", autoClose: 3000,toastId:"Cart-update" });
+      dispatch(removeSuccess());
+    }
+  }, [dispatch, success, message])
+
+
 
   return (
 
@@ -26,14 +61,14 @@ const CartItem = ({ item }) => {
       </div>
 
       <div className="quantity-controls">
-        <button className="quantity-button decrease-btn" onClick={decreaseQuantity}>-</button>
+        <button className="quantity-button decrease-btn" onClick={decreaseQuantity} disabled={loading}>-</button>
         <input
           type="number"
           className="quantity-input"
           value={quantity}
           readOnly
         />
-        <button className="quantity-button increase-btn" onClick={increaseQuantity}>+</button>
+        <button className="quantity-button increase-btn" onClick={increaseQuantity} disabled={loading}>+</button>
       </div>
 
       <div className="item-total">
@@ -41,8 +76,8 @@ const CartItem = ({ item }) => {
       </div>
 
       <div className="item-actions">
-        <button className="update-item-btn">Update</button>
-        <button className="remove-item-btn">Remove</button>
+        <button className="update-item-btn" disabled={loading || quantity === item.quantity} onClick={handleUpdateQuantity}>{loading ? "Updating" : "Update"}</button>
+        <button className="remove-item-btn" >Remove</button>
       </div>
     </div>
   );
