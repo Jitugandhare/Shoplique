b6c509a9feeff90fc42eddc39bf3f98637312498
Jitu gandhare <jitugandhare@gmail.com>
2025-08-04 15:49:58 +0530

    its done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,45 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server:{
-//     proxy:{
-//       '/api':{
-//         target:"http://localhost:8000/"
-//       }
-//     }
-//   }
-// })
-
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true
-      }
-    }
-  },
-  build: {
-    chunkSizeWarningLimit: 1000, // Raise limit or remove the warning
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            // Example: Separate React and vendor packages
-            if (id.includes('react')) return 'react';
-            return 'vendor';
-          }
-        }
+  server:{
+    proxy:{
+      '/api':{
+        target:"http://localhost:8000/"
       }
     }
   }
-});
+})
+
+
+
+
